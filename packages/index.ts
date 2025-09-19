45db23ff92d5fcced5dbd7bcbd3320bdb52986cc
ruixutong.manee <ruixutong.manee@gmail.com>
2025-09-19 12:09:18 +0800

    feat(validator): export

--- packages/index.ts
+++ packages/index.ts
@@ -37,3 +37,7 @@ export type {
   IHookStateSetAction, 
   IHookStateResolvable 
 } from './store';
+
+
+export { BaseValidator } from "./validator";
+export { VRequired, VString, VNumber, VEmail, VMinLength, VArray, VBoolean, VPattern, VMaxLength, VMax, VMin } from './validator';
---
+++ packages/validator/index.ts
@@ -0,0 +1,2 @@
+export { BaseValidator } from "./validator";
+export { VRequired, VString, VNumber, VEmail, VMinLength, VArray, VBoolean, VPattern, VMaxLength, VMax, VMin } from './decorators';
